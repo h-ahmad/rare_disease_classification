ac2c8de8ffcef0d19b58eddf53adc781a13bef51
Hussain Ahmad <hamadni@yahoo.com>
2025-08-17 22:49:52 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 12 17:02:50 2025
+
+@author: hussain
+"""
+
+import configargparse
+import os
+import torch
+from torch.utils.data import DataLoader, Subset
+from sklearn.model_selection import StratifiedShuffleSplit
+import torchvision
+import torch.nn as nn
+import utils
+import numpy as np
+from torch.optim.lr_scheduler import CosineAnnealingLR
+
+if __name__ == "__main__":
+    p = configargparse.ArgumentParser()
+    p.add_argument('--dataset_train', type=str, default='./generation/generate/output_lung_50_shot/lung/sd2.1/gs2.0_nis50/shot50_seed0_template1_lr0.0001_ep300/train/', help='Dataset train path.')
+    p.add_argument('--limit_train_samples', type=bool, default=False, help='subset of total train set.')
+    p.add_argument('--train_samples', type=int, default=700, help='No. of random training samples.')
+    p.add_argument('--dataset_val', type=str, default='./generation/generate/output_lung_50_shot/lung/sd2.1/gs2.0_nis50/shot50_seed0_template1_lr0.0001_ep300/val/', help='Dataset validation path.')
+    p.add_argument('--dataset_test', type=str, default='../../../../data/rare_disease/public/lc25000_lung_colon_image_set/lung_test/', help='Dataset test path.')
+    p.add_argument('--log_dir', type=str, default='output_lung_50_shot', help='Name of the folder to save the model.')
+    p.add_argument('--batch_size', type=int, default=64, help='Batch size.')
+    p.add_argument('--learning_rate', type=float, default=1e-4, help='Learning rate. 1e-3, 1e-4, 5e-5')
+    p.add_argument('--num_epochs', type=int, default=50, help='Number of epochs.')
+    p.add_argument('--device', type=str, default='gpu', help='Choose the device: "gpu" or "cpu"')
+    p.add_argument('--model', type=str, default='resnet50', help='maxvit, swin, efficientnet, resnet50, mobilenetv2, densenet121, resnet101, deit_large, deit_base, volo_d1_224, clip_without_lora , clip_with_lora')
+    # clip and lora setting
+    p.add_argument('--dataset_name', type=str, default='colon', help='scabies, colon etc.')
+    p.add_argument('--clip_download_dir', type=str, default='model/', help='Download clip weights here.')
+    p.add_argument('--clip_version', type=str, default='ViT-B/32', help='ViT-B/16, ViT-B/32')
+    p.add_argument('--is_lora_image', type=bool, default=True)
+    p.add_argument('--is_lora_text', type=bool, default=True)
+    opt = p.parse_args()
+    
+    if opt.device == 'gpu' and torch.cuda.is_available():
+        device = torch.device("cuda:0")
+        print('Device assigned: GPU (' + torch.cuda.get_device_name(device) + ')\n')
+    else:
+        device = torch.device("cpu")
+        if not torch.cuda.is_available() and opt.device == 'gpu':
+            print('GPU not available, device assigned: CPU\n')
+        else:
+            print('Device assigned: CPU\n')
+    
+    transformer = utils.get_transformation(opt)
+    
+    full_train_dataset = torchvision.datasets.ImageFolder(opt.dataset_train, transform=transformer)
+    if opt.limit_train_samples:
+        targets = full_train_dataset.targets
+        splitter = StratifiedShuffleSplit(n_splits=1, train_size=opt.train_samples, random_state=42)
+        for train_idx, _ in splitter.split(np.zeros(len(targets)), targets):
+            full_train_dataset = Subset(full_train_dataset, train_idx)
+            
+    train_loader = DataLoader(full_train_dataset, batch_size=opt.batch_size, shuffle=True)
+    
+    valid_loader = DataLoader(
+        torchvision.datasets.ImageFolder(opt.dataset_val, transform=transformer),
+        batch_size=opt.batch_size, shuffle=False)
+    
+    test_loader = DataLoader(
+        torchvision.datasets.ImageFolder(opt.dataset_test, transform=transformer),
+        batch_size=opt.batch_size, shuffle=False)
+    
+    os.makedirs(opt.log_dir, exist_ok=True)
+    os.makedirs(os.path.join(opt.log_dir, opt.model), exist_ok=True)
+    
+    model = utils.get_model(opt, device)
+    optimiser = torch.optim.AdamW(model.parameters(), lr=opt.learning_rate, weight_decay=1e-4)
+    loss_fn = nn.CrossEntropyLoss()
+    scheduler = CosineAnnealingLR(optimiser, T_max=opt.num_epochs)
+    
+    utils.train(opt.model, model, train_loader, valid_loader, loss_fn, optimiser, opt.num_epochs,
+          opt.batch_size, opt.learning_rate, device, opt.log_dir, scheduler)
+    utils.test(opt.log_dir, opt.model, model, test_loader, device, len(os.listdir(opt.dataset_val)))
---
+++ utils.py
@@ -0,0 +1,294 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 23 23:34:28 2025
+
+@author: hussain
+"""
+
+import os
+import torch
+from torchvision.models import maxvit_t, MaxVit_T_Weights
+import clip
+from model.ConvNet import ConvNet
+from model.SwinTransformer import SwinTransformer
+from model.clip import CLIP
+from torchvision import models
+import torch.nn as nn
+import numpy as np
+from tqdm import tqdm
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, roc_auc_score, confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+import timm
+from torchvision.transforms import transforms
+
+CLIP_MEAN=(0.48145466, 0.4578275, 0.40821073)
+CLIP_STD=(0.26862954, 0.26130258, 0.27577711)
+
+NORM_MEAN = (0.485, 0.456, 0.406)
+NORM_STD = (0.229, 0.224, 0.225)
+
+def get_transformation(opt):
+    img_crop_size = 0.0
+    if(opt.model == 'convnet'): 
+        img_crop_size = 150 
+    else: img_crop_size = 224
+    
+    if(opt.model == 'clip_with_lora' or opt.model == 'clip_without_lora'):
+        transformer = transforms.Compose([        
+            transforms.RandomResizedCrop(img_crop_size), # for convnet, 150, while for others, 224
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(15),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+            transforms.ToTensor(),
+            transforms.Normalize(CLIP_MEAN, CLIP_STD),
+            ])
+    elif opt.model == 'resnet50':
+        transformer = transforms.Compose([        
+            transforms.RandomResizedCrop(img_crop_size), # for convnet, 150, while for others, 224
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(15),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+            transforms.ToTensor(),
+            transforms.Normalize(NORM_MEAN, NORM_STD),
+            ])
+    else:
+        transformer = transforms.Compose([        
+            transforms.RandomResizedCrop(img_crop_size), # for convnet, 150, while for others, 224
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(15),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5,0.5,0.5], # 0-1 to [-1,1]
+                                [0.5,0.5,0.5]),
+            ])
+    
+    return transformer
+
+class CLIPFineTuner(nn.Module):
+    def __init__(self, encoder, out_dim):
+        super().__init__()
+        self.encoder = encoder
+        self.fc = nn.Linear(512, out_dim)  # 512 for ViT-B/32
+
+    def forward(self, x):
+        x = self.encoder(x)  # [B, 512]
+        return self.fc(x)
+
+def compute_accuracy(y_pred, y):
+    top_pred = y_pred.argmax(1, keepdim=True)
+    correct = top_pred.eq(y.view_as(top_pred)).sum()
+    acc = correct.float() / y.shape[0]
+    return acc
+
+def get_model(opt, device):
+    if opt.model == 'convnet':
+        model = ConvNet(num_classes=len(os.listdir(opt.dataset_train)))
+    elif opt.model == 'maxvit':
+        model = maxvit_t(weights=MaxVit_T_Weights.IMAGENET1K_V1)
+        model.classifier = torch.nn.Sequential(torch.nn.AdaptiveAvgPool2d(1),
+                                               torch.nn.Flatten(),
+                                               torch.nn.LayerNorm(512),
+                                               torch.nn.Linear(512, 512),
+                                               torch.nn.Tanh(),
+                                               torch.nn.Linear(512, len(os.listdir(opt.dataset_train)), bias=False),)
+    elif opt.model == 'swin':
+        model = SwinTransformer(num_classes=len(os.listdir(opt.dataset_train)))
+    elif opt.model == 'efficientnet':
+        model = models.efficientnet_b0(pretrained=True)
+        model.classifier[1] = nn.Linear(model.classifier[1].in_features, len(os.listdir(opt.dataset_train)))
+    elif opt.model == 'resnet50':
+        model = models.resnet50(pretrained=True)
+        model.fc = nn.Linear(model.fc.in_features, len(os.listdir(opt.dataset_train)))
+    elif opt.model == 'mobilenetv2':
+        model = models.mobilenet_v2(pretrained=True)
+        model.classifier[1] = nn.Linear(model.last_channel, len(os.listdir(opt.dataset_train)))
+    elif opt.model == 'densenet121':
+        model = models.densenet121(pretrained=True)
+        model.classifier = nn.Linear(model.classifier.in_features, len(os.listdir(opt.dataset_train)))
+    elif opt.model == 'resnet101':
+        model = models.resnet101(pretrained=True)
+        model.fc = nn.Linear(model.fc.in_features, len(os.listdir(opt.dataset_train))) 
+    elif opt.model == 'deit_large':
+        model = timm.create_model('deit3_large_patch16_224', pretrained=True)
+        model.head = nn.Linear(model.head.in_features, len(os.listdir(opt.dataset_train))) 
+    elif opt.model == 'deit_base':
+        model = timm.create_model('deit3_base_patch16_224', pretrained=True)
+        model.head = nn.Linear(model.head.in_features, len(os.listdir(opt.dataset_train)))  # Adjust final layer
+    elif opt.model == 'volo_d1_224':
+        model = timm.create_model('volo_d1_224', pretrained=True)
+        model.reset_classifier(num_classes=len(os.listdir(opt.dataset_train)))
+    elif opt.model == 'clip_without_lora':
+        model, preprocess = clip.load(opt.clip_version, device=device)
+        image_encoder = model.visual  
+        for param in image_encoder.parameters():
+            param.requires_grad = True      
+        model = CLIPFineTuner(image_encoder, out_dim = len(os.listdir(opt.dataset_train)))
+        model = model.to(device).float()
+    elif opt.model == 'clip_with_lora':
+        model = CLIP(dataset=opt.dataset_name, is_lora_image=opt.is_lora_image, is_lora_text=opt.is_lora_text,
+                     clip_download_dir=opt.clip_download_dir, clip_version=opt.clip_version,)
+        model = model.to(device).float()
+    
+    if (opt.model != 'clip_without_lora' and opt.model != 'clip_with_lora'):
+        for param in model.parameters():
+            param.requires_grad = True          
+        model = model.to(device)
+        
+    return model
+
+def train(model_name, model, train_loader, validate_loader, loss_fn, optimiser, num_epochs,
+          batch_size, learning_rate, device, log_dir, scheduler):
+    max_val_acc = 0.0
+    train_accs = []
+    train_losses = []
+    val_accs = []
+    val_losses = []
+    bestEpoch = 0
+    print('--------------------------------------------------------------')
+    # Loop along epochs to do the training
+    for i in range(num_epochs):
+        train_accuracy = 0.0
+        train_loss = 0.0
+        print('\nTRAINING')     
+        model.train()
+        for images, labels in train_loader:
+            print('\rEpoch[' + str(i+1) + '/' + str(num_epochs) + ']: ', end='')
+            images, labels = images.to(device), labels.to(device)
+            labels = labels.to(device)
+            optimiser.zero_grad()
+            predictions = model(images)
+            loss = loss_fn(predictions, labels)
+            loss.backward()
+            optimiser.step()
+            scheduler.step()
+            train_accuracy += compute_accuracy(predictions, labels).item()
+            train_loss += loss.item()
+            
+        # validation
+        val_accuracy = 0.0
+        val_loss = 0.0
+        model.eval()
+        print('\n\nVALIDATION')
+        for images, labels in validate_loader:
+            print('\rEpoch[' + str(i+1) + '/' + str(num_epochs) + ']: ', end='')            
+            images, labels = images.to(device), labels.to(device)
+            predictions = model(images)
+            loss = loss_fn(predictions, labels)
+            val_accuracy += compute_accuracy(predictions, labels).item()
+            val_loss += loss.item()
+            
+        train_accs.append(train_accuracy / len(train_loader))
+        val_accs.append(val_accuracy / len(validate_loader))
+        train_losses.append(train_loss / len(train_loader))
+        val_losses.append(val_loss / len(validate_loader))
+            
+        if (val_accuracy / len(validate_loader)) > max_val_acc:
+            torch.save(model.state_dict(), log_dir+"/"+model_name+"/checkpoint.pth")
+            print('\n')
+            print(f'- Train Acc: {(train_accuracy / len(train_loader))*100:.2f}%')
+            print(f'- Val Acc: {(val_accuracy / len(validate_loader))*100:.2f}%')
+            print(f'- Train Loss: {train_loss / len(train_loader):.3f}')
+            print(f'- Val Loss: {val_loss / len(validate_loader):.3f}')
+            print(f'\nAccuracy increased ({max_val_acc*100:.6f}% ---> {(val_accuracy / len(validate_loader))*100:.6f}%) \nModel saved')
+            max_val_acc = val_accuracy / len(validate_loader)
+            bestEpoch = i
+        print("--------------------------------------------------------------")
+        # Save losses and accuracies
+        # max_val_acc #.... within each epoch
+    # out of epoch
+    plot_train_acc_loss(log_dir, model_name, train_accs, val_accs, train_losses, val_losses, bestEpoch)
+    
+def test(output_path, model_name, model, dataloader, device, num_classes, is_multiclass=False):
+    state_dict = torch.load(output_path+"/"+model_name+'/checkpoint.pth', map_location=device)
+    model.load_state_dict(state_dict)
+    model.eval()
+    all_labels = []
+    all_preds = []
+    all_probs = []
+    test_accuracy = 0.0
+    with torch.no_grad():
+        for inputs, labels in tqdm(dataloader, desc="Evaluating"):
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            probs = torch.softmax(outputs, dim=1)
+            preds = torch.argmax(probs, dim=1)
+            
+            all_labels.extend(labels.cpu().numpy())
+            all_preds.extend(preds.cpu().numpy())
+            all_probs.extend(probs.cpu().numpy())
+    # Convert to numpy arrays
+    all_labels = torch.tensor(all_labels).numpy()
+    all_preds = torch.tensor(all_preds).numpy()
+    all_probs = torch.tensor(all_probs).numpy()
+    # === Classification Metrics ===
+    f1 = f1_score(all_labels, all_preds, average='macro')
+    precision = precision_score(all_labels, all_preds, average='macro')
+    recall = recall_score(all_labels, all_preds, average='macro')
+    accuracy = accuracy_score(all_labels, all_preds)
+    try:
+        # For multiclass AUROC, we use one-vs-rest
+        if num_classes > 2:
+            from sklearn.preprocessing import label_binarize
+            binary_labels = label_binarize(all_labels, classes=list(range(num_classes)))
+            auroc = roc_auc_score(binary_labels, all_probs, average='macro', multi_class='ovr')
+        else:
+            auroc = roc_auc_score(all_labels, [p[1] for p in all_probs])
+    except Exception as e:
+        auroc = f"Could not calculate AUROC: {str(e)}"
+
+    cm = confusion_matrix(all_labels, all_preds)
+
+    print(f"\n=== Evaluation Metrics ===")
+    print(f"Accuracy     : {accuracy:.4f}")
+    print(f"F1 Score     : {f1:.4f}")
+    print(f"Precision    : {precision:.4f}")
+    print(f"Recall       : {recall:.4f}")
+    print(f"AUROC        : {auroc}")
+    print(f"Confusion Matrix:\n{cm}")  
+    
+    metrics_file = os.path.join(output_path, model_name, "evaluation_metrics.txt")
+    with open(metrics_file, "w") as f:
+        f.write(f"Accuracy: {accuracy:.4f}\n")
+        f.write(f"Precision: {precision:.4f}\n")
+        f.write(f"Recall: {recall:.4f}\n")
+        f.write(f"F1 Score: {f1:.4f}\n")
+        f.write(f"AUROC: {auroc:.4f}\n")
+        print(f"Metrics saved to {metrics_file}")
+        
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['lung_aca', 'lung_n', 'lung_scc'])
+    disp.plot(cmap=plt.cm.Blues)
+    plt.title("Confusion Matrix")
+    plt.xlabel("Predicted Label")
+    plt.ylabel("True Label")
+    plt.grid(False)
+    plt.savefig(os.path.join(output_path, model_name, 'confusion_matrix.pdf'))
+        
+def plot_train_acc_loss(log_dir, model_name, train_acc, val_acc, train_loss, val_loss, best_epoch):
+    epochs = np.arange(torch.tensor(train_loss).shape[0])
+    
+    plt.figure()
+    plt.plot(epochs, train_loss, label="Training loss", c='b')
+    plt.plot(epochs, val_loss, label="Validation loss", c='r')
+    plt.plot(best_epoch, val_loss[best_epoch], label="Best epoch", c='y', marker='.', markersize=10)
+    plt.text(best_epoch+.01, val_loss[best_epoch]+.01, str(best_epoch) + ' - ' + str(round(val_loss[best_epoch], 3)), fontsize=8)
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.title('Loss along epochs')
+    plt.legend()
+    plt.draw()
+    plt.savefig(os.path.join(log_dir, model_name, 'loss.pdf'))
+    
+    plt.figure()
+    plt.plot(epochs, train_acc, label="Training accuracy", c='b')
+    plt.plot(epochs, val_acc, label="Validation accuracy", c='r')
+    plt.plot(best_epoch, val_acc[best_epoch], label="Best epoch", c='y', marker='.', markersize=10)
+    plt.text(best_epoch+.001, val_acc[best_epoch]+.001, str(best_epoch) + ' - ' + str(round(val_acc[best_epoch], 3)), fontsize=8)
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.title('Accuracy along epochs')
+    plt.legend()
+    plt.draw()
+    plt.savefig(os.path.join(log_dir, model_name, 'accuracy.pdf'))
+    
+    # plt.show()
+        
